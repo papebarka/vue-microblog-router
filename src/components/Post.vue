85cee01a59d194bc75a1fb484e6a33aa4c358daa
Pape Barka <papebarka@gmail.com>
2023-08-08 05:54:28 +0000

    Dynamic render of post

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,11 +1,15 @@
 <script setup>
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { testPosts } from '../data/posts.js'
 
 const route = useRoute()
 
-const post = testPosts.find( elt => elt.id === parseInt(route.params.id, 10))
+const getPost = computed( () => {
+    return testPosts.find( elt => elt.id === parseInt(route.params.id, 10))
+})
 
+const post = getPost
 </script>
 
 <template>
